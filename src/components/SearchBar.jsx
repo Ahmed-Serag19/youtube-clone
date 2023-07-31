17f148a9b65c61c4e7152f7bca016ef3483a2b39
Ahmed Mohamed <ahmedmohamed.amin@hotmail.com>
2023-07-31 11:19:56 +0300

    so i can continue from laptop

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,36 @@
+import { IconButton, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Search } from '@mui/icons-material';
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxshadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton
+        aria-label="search feed button"
+        type="submit"
+        sx={{ p: '10px', color: 'red' }}
+      >
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,27 @@
+import { Box, Stack } from '@mui/material';
+import React from 'react';
+import { VideoCard, ChannelCard } from './';
+
+const Videos = ({ videos }) => {
+  return (
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos.map((item, index) => {
+        return (
+          <Box key={index}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {item.id.channelId && (
+              <ChannelCard channelDetail={item} />
+            )}
+          </Box>
+        );
+      })}
+    </Stack>
+  );
+};
+
+export default Videos;
