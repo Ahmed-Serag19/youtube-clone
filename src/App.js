86faa4b3053fb5aa7195e33c1bef819ea7b35abe
Ahmed Mohamed <ahmedmohamed.amin@hotmail.com>
2023-08-03 16:48:57 +0300

    adjusting routes with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  HashRouter,
+} from 'react-router-dom';
 import { Box } from '@mui/material';
 import {
   ChannelDetail,
@@ -13,21 +18,27 @@ const App = () => {
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: '#000' }}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/video/:id" exact element={<VideoDetail />} />
-          <Route
-            path="/channel/:id"
-            exact
-            element={<ChannelDetail />}
-          />
-          <Route
-            path="/search/:searchTerm"
-            exact
-            element={<SearchFeed />}
-          />
-        </Routes>
+        <HashRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route
+              path="/video/:id"
+              exact
+              element={<VideoDetail />}
+            />
+            <Route
+              path="/channel/:id"
+              exact
+              element={<ChannelDetail />}
+            />
+            <Route
+              path="/search/:searchTerm"
+              exact
+              element={<SearchFeed />}
+            />
+          </Routes>
+        </HashRouter>
       </Box>
     </BrowserRouter>
   );
